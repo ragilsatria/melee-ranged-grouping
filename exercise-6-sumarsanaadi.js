9cf404eb8467c0c1e822b2cc430e125722b8701a
sumarsanaadi <sumarsana.adi@gmail.com>
2018-05-17 10:34:11 +0700

    add exec 6 week 5 sumarsanaadi

--- exercise-6-sumarsanaadi.js
+++ exercise-6-sumarsanaadi.js
@@ -12,23 +12,27 @@ Jika input adalah string kosong ('') maka return array kosong
 */
 
 function meleeRangedGrouping (str) {
-  var output = []; // inisiasi var output sebagai array kosong
+  var output = [[],[]]; // inisiasi var output sebagai array 2 dimensi kosong untuk menampung melee dan ranged
   var split1 = str.split(','); //pertama kita split dahulu input string dengan karakter ',' disimpan di variable split1
-  for (var i = 0 ; i < split1.length ; i++) { //lakukan perulangan sepanjang array split1
-    var split2 = split1[i].split('-'); //split lagi tiap value index split1 dengan karakter '-' , dan simpan di variabel split2
-    if (split2[1] === "Ranged") { //setelah di split lagi, bandingkan value array kedua apakah ada 'Ranged' jika iya masukkan ke array output index pertama
-      if (output[0] === undefined) {
-        output[0] = [];
+  if (str.length === 0) { //cek kondisi saat input empty string sesuai test case
+    output = [];
+  } else {
+    for (var i = 0 ; i < split1.length ; i++) { //lakukan perulangan sepanjang array split1
+      var split2 = split1[i].split('-'); //split lagi tiap value index split1 dengan karakter '-' , dan simpan di variabel split2
+      if (split2[1] === "Ranged") { //setelah di split lagi, bandingkan value array kedua apakah ada 'Ranged' jika iya masukkan ke array output index pertama
+        if (output[0] === undefined) {
+          output[0] = [];
+        }
+        output[0].push(split2[0]);
+      } else if(split2[1] === "Melee") { //setelah di split lagi, bandingkan value array kedua apakah ada 'Melee' jika iya masukkan ke array output index kedua
+        if (output[1] === undefined) {
+          output[1] = [];
+        }
+        output[1].push(split2[0]);
       }
-      output[0].push(split2[0]);
-    } else if(split2[1] === "Melee") { //setelah di split lagi, bandingkan value array kedua apakah ada 'Melee' jika iya masukkan ke array output index kedua
-      if (output[1] === undefined) {
-        output[1] = [];
-      }
-      output[1].push(split2[0]);
     }
   }
-    return output;
+  return output;
 }
 
 // TEST CASE
