f62a6bcdb565f2996b4e5e335179c85c9ca53cce
Maram <lockonmaram@fsociety-s451lb.local>
2018-05-23 10:36:32 +0700

    range.js added

---
+++ 6.range/range.js
@@ -0,0 +1,24 @@
+function meleeRangedGrouping (str) {
+  var split = str.split(',');
+  var arr = [[],[]];
+
+  for (var i = 0; i < split.length; i++) {
+    if (split[i].split('-')[1] === 'Ranged') {
+      arr[0].push(split[i].split('-')[0]);
+    }else if (split[i].split('-')[1] === 'Melee') {
+      arr[1].push(split[i].split('-')[0]);
+    }
+  }
+
+  return arr;
+}
+
+// TEST CASE
+
+console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+// [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+
+console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+// [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+
+console.log(meleeRangedGrouping('')); // []
