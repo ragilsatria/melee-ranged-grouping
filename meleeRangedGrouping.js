fba70c743a557988683b4f0dc2e545a80d5c0303
Mahbub <prof.bub@live.com>
2019-01-28 12:20:38 +0700

    Added meleeRangedGrouping.js

---
+++ meleeRangedGrouping.js
@@ -0,0 +1,57 @@
+/*
+Logic Challenge: Melee Ranged Grouping
+
+
+Diberikan function meleeRangedGrouping yang menerima 1 parameter berupa string, implementasikan meleeRangedGrouping agar dapat menghasilkan multidimensional array seperti yang diminta.
+
+Format string yang diberikan adalah: <nama_hero>-<tipe_hero>,<nama_hero>-<tipe-hero>, ...
+
+Output yang diharapkan: [ [ <daftar_hero_dengan_tipe_ranged> ], [ <daftar_hero_dengan_tipe_melee> ] ]
+
+Jika input adalah string kosong ('') maka return array kosong
+
+Submit Tugas Disini
+
+function meleeRangedGrouping (str) {
+  //your code here
+}
+
+// TEST CASE
+
+console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+// [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+
+console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+// [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+
+console.log(meleeRangedGrouping('')); // []
+*/
+
+function meleeRangedGrouping(str) {
+    //your code here
+    if (typeof str !== 'string' || str.length < 1) return [];
+
+    let output = [[], []];
+
+    str = str.split(',');
+    str.forEach(function (item) {
+        if (!!item.split('-')[1] && (item.split('-')[1]).toLowerCase() === 'ranged') {
+            output[0].push(item.split('-')[0])
+        } else if (!!item.split('-')[1] && (item.split('-')[1]).toLowerCase() === 'melee') {
+            output[1].push(item.split('-')[0])
+        }
+    });
+
+    return output
+
+}
+
+// TEST CASE
+
+console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+// [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+
+console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+// [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+
+console.log(meleeRangedGrouping('')); // []
