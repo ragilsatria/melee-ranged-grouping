9bc8484b10627c205712f05d3fe502e2b4629ce9
rizkyanas35 <rizkyanas25@gmail.com>
2019-03-21 12:09:30 +0700

    update salah file

--- exercise6.js
+++ exercise6.js
@@ -1,16 +1,46 @@
-function attack (damage) {
-  // Code disini
-  return damage - 2
-}
+function meleeRangedGrouping (str) {
+  //your code here
+  if (str.length === 0 || str === undefined) {
+    return []
+  }
+  var result = []
+  var arr = str.split(',')
+  var newArr = []
+  var obj = {}
+  for (var i = 0; i < arr.length; i++) {
+    newArr.push(arr[i].split('-'))
+  }
 
-function damageCalculation (numberOfAttacks, damagePerAttack) {
-  // Code disini
-  return numberOfAttacks * attack(damagePerAttack)
+  for (var i = 0; i < newArr.length; i++) {
+    if (obj[newArr[i][1]] === undefined) {
+      obj[newArr[i][1]] = []
+    }
+    obj[newArr[i][1]].push(newArr[i][0])
+  }
+  
+  var key = Object.keys(obj)
+  for (var i = 0; i < key.length; i++) {
+    result.push(obj[key[i]])
+  }
+
+  // for (var key in obj) {
+  //   result.push(obj[key])
+  // } 
+
+  if (result.length < 2) {
+    result.push([])
+  }
+  console.log(obj)
+  console.log(key)
+  return result
 }
 
 // TEST CASE
-console.log(damageCalculation(9, 25)); // 207
 
-console.log(damageCalculation(10, 4)); // 20
+console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+// [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
 
-console.log(damageCalculation(5, 20)); // 90
+console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+// [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+
+console.log(meleeRangedGrouping('')); // []
