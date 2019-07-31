72120156b3c030d0438f757c4d517f40eb714c72
laskarks <laskar.ksatria12@gmail.com>
2019-07-31 07:09:38 -0400

    Tugas Melee

---
+++ MeleeRanging.js
@@ -0,0 +1,109 @@
+// Logic Challenge: Melee Ranged Grouping
+
+// Diberikan function meleeRangedGrouping yang menerima 1 parameter berupa string, implementasikan meleeRangedGrouping 
+// agar dapat menghasilkan multidimensional array seperti yang diminta.
+
+// Format string yang diberikan adalah: <nama_hero>-<tipe_hero>,<nama_hero>-<tipe-hero>, ...
+
+// Output yang diharapkan: [ [ <daftar_hero_dengan_tipe_ranged> ], [ <daftar_hero_dengan_tipe_melee> ] ]
+
+// Jika input adalah string kosong ('') maka return array kosong
+
+function meleeRangedGrouping(str) {
+
+
+    let str1 = '';
+    let str2 = '';
+    let arr1 = [];
+    let arr2 = [];
+    let s = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === ',') {
+            arr1.push(str1);
+            arr2.push(str2);
+            s = 0;
+            str1 = '';
+            str2 = ''
+        } else {
+            if (str[i] === '-') {
+                s++
+            }
+            if (s === 0 && str[i] != '-') {
+                str1 += str[i]
+            } else if (s > 0 && str[i] != '-') {
+                str2 += str[i]
+            }
+        }
+    }
+
+    return arr2
+
+
+
+
+
+
+
+
+    // str=str+","
+    // var arrRes=[[],[]];
+    // var arrHero=[];
+    // var arrTipe=[];
+    // var strTemp='';
+    // var strTemp2='';
+    // var arrTemp=[];
+
+    // for(var i = 0; i<str.length; i++){
+    //     if(str[i]!==","){
+    //         strTemp+=str[i];
+    //     }else {
+    //         arrHero.push(strTemp);
+    //         strTemp='';
+    //     }
+    // }
+    // // console.log(arrHero);
+
+    // for(var j=0; j<arrHero.length; j++){
+    //     arrTipe.push([]);
+    //     for(var k=0; k<arrHero[j].length; k++){
+    //         if(arrHero[j][k]==="-"){
+    //             arrTipe[j].push(strTemp2);
+    //             strTemp2='';
+    //         }else if(k===arrHero[j].length-1){
+    //             arrTipe[j].push(strTemp2)
+    //             strTemp2='';
+    //         }else {
+    //             strTemp2+=arrHero[j][k]
+    //         }
+    //     }
+    // }
+    // // console.log(arrTipe);
+
+    // for(a=0; a<arrTipe.length; a++){
+    //     for(b=0; b<arrTipe[a].length; b++){
+    //         if(arrTipe[a][b]==="Range"){
+    //             arrRes[0].push(arrTipe[a][0]);
+    //         }if(arrTipe[a][b]==="Mele"){
+    //             arrRes[1].push(arrTipe[a][0]);
+    //         }
+    //     }
+    // }
+
+    // // console.log(arrRes);
+    // if(str.length===1){
+    //   return []
+    // }
+
+    // return arrRes  
+}
+
+// TEST CASE
+
+console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+// [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+
+console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+// [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+
+console.log(meleeRangedGrouping('')); // []
