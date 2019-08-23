3038077139e01f1feb2d04097a8c3ca8d4abcd63
devitasari <devitas700@gmail.com>
2019-08-23 16:32:21 +0700

    add new file

---
+++ exercise7.js
@@ -0,0 +1,27 @@
+function meleeRangedGrouping (str) {
+    //your code here
+    if (!str) return []
+    var output = [[],[]]
+    str1 = str.split(',')
+    // console.log(str1)
+    for (var i=0; i<str1.length; i++) {
+        str2 = str1[i].split('-')
+        if (str2[1] === 'Ranged') {
+            output[0].push(str2[0])
+        } else {
+            output[1].push(str2[0])
+        }
+    }
+    // console.log(str2)
+    return output
+  }
+  
+  // TEST CASE
+  
+  console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+  // [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+  
+  console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+  // [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+  
+  console.log(meleeRangedGrouping('')); // []
