6e67911326bb150ac8c132b7909e4c1dd9e57fcb
Your Name <you@example.com>
2019-11-16 19:38:40 +0700

    Tugas Melee Ranged Grouping oleh Maulid

--- Melee_Ranged_Grouping.js
+++ Melee_Ranged_Grouping.js
@@ -22,13 +22,13 @@ function meleeRangedGrouping (str) {
     }
     return heroK
 }
-  
-  // TEST CASE
-  
-  console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
-  // [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
-  
-  console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
-  // [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
-  
-  console.log(meleeRangedGrouping('')); // []
+
+// TEST CASE
+
+console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+// [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+
+console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+// [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+
+console.log(meleeRangedGrouping('')); // []
