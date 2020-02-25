d24466ed4f0bf85ea9234f7607e0bfa74761fcba
abudiar <alanwbudiarto@gmail.com>
2020-02-25 15:16:56 +0700

    Replaced damageCalculation w/ meleeRangedGrouping

--- main.js
+++ main.js
@@ -1,14 +1,59 @@
-function attack(damage) {
-    return damage - 2;
-}
-
-function damageCalculation(numberOfAttacks, damagePerAttack) {
-    return numberOfAttacks * attack(damagePerAttack);
+function meleeRangedGrouping(str) {
+    var result = [];
+    if (str[0] != null) {
+        var text = "";
+        var hold = [];
+        var ranged = [];
+        var melee = [];
+        for (var i in str) {
+            if (str[i] == ",") {
+                hold.push(text);
+                text = "";
+            }
+            else if (i == str.length - 1) {
+                text += str[i];
+                hold.push(text);
+                text = "";
+            } else
+                text += str[i];
+        }
+        for (var i in hold) {
+            text = "";
+            var name = "";
+            var type = "";
+            for (var j in hold[i]) {
+                if (hold[i][j] == "-") {
+                    name = text;
+                    text = "";
+                } else if (hold[i][j] == ",") {
+                    type = text;
+                    text = "";
+                } else if (j == hold[i].length - 1) {
+                    text += hold[i][j];
+                    type = text;
+                    text = "";
+                } else
+                    text += hold[i][j];
+            }
+            if (type == "Ranged") {
+                ranged.push(name);
+            }
+            if (type == "Melee") {
+                melee.push(name);
+            }
+        }
+        result.push(ranged);
+        result.push(melee);
+    }
+    return result;
 }
 
 // TEST CASE
-console.log(damageCalculation(9, 25)); // 207
 
-console.log(damageCalculation(10, 4)); // 20
+console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+// [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
 
-console.log(damageCalculation(5, 20)); // 90
+console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+// [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+
+console.log(meleeRangedGrouping('')); // []
