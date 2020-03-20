faa2ad311a3960b706b22c4daf9261b8313b60c0
michaelm96 <michaelarceus96@gmail.com>
2020-03-20 19:15:41 +0700

    add exercise-6.js first file

--- exercise-6.js
+++ exercise-6.js
@@ -1,45 +1,45 @@
-function meleeRangedGrouping (str) {
+function meleeRangedGrouping(str) {
     //your code here
-    var hasil =[];
-    if(str.length===0){
+    var hasil = [];
+    if (str.length === 0) {
         return hasil
     }
-    var char =[];
-    var type=[]
-    var tampung='';
+    var char = [];
+    var type = []
+    var tampung = '';
     for (let i = 0; i < str.length; i++) {
-        if(str[i]==='-'){
+        if (str[i] === '-') {
             char.push(tampung);
-            tampung='';
-        }else if (str[i]===','){
+            tampung = '';
+        } else if (str[i] === ',') {
             type.push(tampung)
-            tampung='';
-        }else{
-            tampung+= str[i];
+            tampung = '';
+        } else {
+            tampung += str[i];
         }
     }
     type.push(tampung)
-    var ranged =[]
-    var melee =[]
+    var ranged = []
+    var melee = []
     for (let i = 0; i < char.length; i++) {
-        if(type[i]==='Ranged'){
+        if (type[i] === 'Ranged') {
             ranged.push(char[i])
-        }else{
+        } else {
             melee.push(char[i])
         }
     }
-    hasil.push(ranged,melee)
+    hasil.push(ranged, melee)
 
     return hasil;
 
-  }
-  
-  // TEST CASE
-  
-  console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
-  // [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
-  
-  console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+}
+
+// TEST CASE
+
+console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+// [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+
+console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
 //   // [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
-  
-  console.log(meleeRangedGrouping('')); // []
+
+console.log(meleeRangedGrouping('')); // []
